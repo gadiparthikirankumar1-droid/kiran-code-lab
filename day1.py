434127e5ce006c9a66da071a65334ac6acfa4152
Kiran Kumar Gadiparthi <gadiparthikirankumar1@gmail.com>
2026-08-20 16:47:36 +0530

    day1.py

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+# Check the user age for vote
+
+age = int(input("Enter your age: "))
+if age >= 19:
+    print("you are eligible to vote.")
+else:
+    print("you are not eligible to vote.")
